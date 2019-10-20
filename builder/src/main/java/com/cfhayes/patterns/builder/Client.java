3185339a9b8213499e4e1d1f852c45cabf21e464
Charles Hayes <cfhayes16@gmail.com>
2019-10-19 22:15:05 -0600

    Adding readme file

--- builder/src/main/java/com/cfhayes/patterns/builder/Client.java
+++
@@ -1,7 +0,0 @@
-package com.cfhayes.patterns.builder;
-
-public class Client {
-
-  public static void main(String[] args) {
-  }
-}
--- singleton/src/main/java/com/cfhayes/patterns/singleton/Client.java
+++
@@ -1,7 +0,0 @@
-package com.cfhayes.patterns.singleton;
-
-public class Client {
-
-  public static void main(String[] args) {
-  }
-}
